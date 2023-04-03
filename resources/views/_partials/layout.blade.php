9a17fc64a302ce3dfdd08a83e87864836242cc63
Rocca-Francesco <roccafrance98@yahoo.it>
2023-04-03 16:42:52 +0200

    add navbar - layout

--- resources/views/_partials/layout.blade.php
+++ resources/views/_partials/layout.blade.php
@@ -11,6 +11,7 @@
 </head>
 
 <body>
+  @include('_partials.navbar')
   <div class="container">
     @yield('contentPage')
   </div>
---
+++ resources/views/_partials/navbar.blade.php
@@ -0,0 +1,22 @@
+<div class="bg-dark">
+  <div class="container">
+  <nav class="navbar navbar-expand-lg">
+  <div class="container-fluid ">
+    <a class="navbar-brand text-warning fw-bold" href="#">MusicTIME</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active text-primary" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active text-primary" aria-current="page" href="#">All Tracks</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</div>
+</div>
